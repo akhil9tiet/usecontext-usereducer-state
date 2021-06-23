2b84a8af8f61ae569f1cc7dc36f395794c59e4bf
Akhil <akhil9tiet@gmail.com>
2021-06-23 00:06:22 -0700

    push final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import GlobalStateProvider from './store/GlobalStateProvider';
+import { Nav, DownloadImage, LoginNotification } from './components';
 
 function App() {
-  return (
+	return (
 		<GlobalStateProvider>
 			<div className='App'>
 				<header className='App-header'>
-					<p>Test</p>
+					<Nav />
+					<p>Download Image</p>
+					<DownloadImage />
+					<LoginNotification />
 				</header>
 			</div>
 		</GlobalStateProvider>
-  );
+	);
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import Context from '../store/context';
+
+export const Nav = () => {
+	const { globalState, globalDispatch } = useContext(Context);
+
+	return (
+		<nav>
+			{globalState.isLoggedIn ? (
+				<button type='buttton' onClick={() => globalDispatch({ type: 'LOGOUT' })}>
+					Logout
+				</button>
+			) : (
+				<button type='buttton' onClick={() => globalDispatch({ type: 'LOGIN' })}>
+					Login
+				</button>
+			)}
+		</nav>
+	);
+};
